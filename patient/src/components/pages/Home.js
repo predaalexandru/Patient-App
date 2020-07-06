c1558eb9d21d50283d2b27398d040b369220595b
Preda Alexandru <alexpreda95@yahoo.com>
2020-07-06 20:08:45 +0300

    Creating the table and db json

--- patient/src/components/pages/Home.js
+++ patient/src/components/pages/Home.js
@@ -1,15 +1,51 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
 /**
 * @author
 * @function Home
 **/
 
-const Home = (props) => {
+const Home = () => {
+  const [patients, setPatient] = useState([]);
+
+  useEffect(() => {
+    loadPatient();
+  }, []);
+
+  const loadPatient = async () => {
+    const result = await axios.get("http://localhost:3003/patient");
+    setPatient(result.data);
+  }
+
   return(
     <div className="container">
         <div className="py-4">
             <h1>Home Page</h1>
+
+            <table className="table border shadow">
+  <thead className="thead-dark">
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Nume</th>
+      <th scope="col">Prenume</th>
+      <th scope="col">Localitate</th>
+      <th>Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    {
+      patients.map((patient, index) => (
+        <tr key={patient.id}>
+          <th scope="row">{index + 1}</th>
+          <td>{patient.name}</td>
+          <td>{patient.username}</td>
+          <td>{patient.email}</td>
+        </tr>
+      ))
+    }
+  </tbody>
+</table>
         </div>
     </div>
    )
---
+++ patient/src/components/pages/NotFound.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+/**
+* @author
+* @function NotFound
+**/
+
+const NotFound = (props) => {
+  return(
+    <div className="not-found">
+        <h1 className="display-1">Page Not Found</h1>
+    </div>
+   )
+
+ }
+
+export default NotFound
